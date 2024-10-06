14449d8430cf2a7e7f124eef21e9dbba3f91aef4
develdoe <andreeray@live.com>
2024-10-06 21:32:25 +0200

    update v2.4

--- open.js
+++ open.js
@@ -19,6 +19,8 @@ const alpaca = new Alpaca({
   usePolygon: false
 });
 
+tet
+
 const args = minimist(process.argv.slice(2));  // Parse command-line arguments
 const isVerbose = args['v'];
 const isTestMode = args['n'];  // Check for -n flag
